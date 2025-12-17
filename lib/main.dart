1b54d80ac6b568cdbe5401d329fb1d54d99d337a
MaiiSmart <mai@smarttechno.co>
2025-12-18 00:44:23 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:tasked_in/tasked_in_app.dart';
+
+void main(){
+runApp(TaskedInApp());
+}
---
+++ lib/tasked_in_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TaskedInApp extends StatelessWidget {
+  const TaskedInApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
